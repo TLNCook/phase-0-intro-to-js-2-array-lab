d24d8aa8211e3a04a5c8767c2d7935b94c05ecae
Thomas Cook <tlncook85@gmail.com>
2022-05-17 20:00:57 -0600

    commit tree dbc62eb1ce4f0c44a2f42ba0797a7bf269316440

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+function appendCat() {
+   const newCats = [...cats, "Broom"];
+   return newCats;
+}
+function prependCat() {
+    const newCats = ["Arnold", ...cats];
+    return newCats;
+}
+function removeLastCat() {
+    return cats.slice(0, -1);
+}
+function removeFirstCat() {
+    return cats.slice(1);
+
+}
